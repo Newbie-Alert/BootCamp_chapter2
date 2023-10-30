1fb567d9390f340721ba5057bf41499e0544ea81
NebieAlert <mjnn100801@naver.com>
2023-10-30 13:31:19 +0900

    fetch 에러 처리, 함수마다 중복되는 fetch 수정

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const movieContainer = document.querySelector('.movie_container');
-const movieCard = document.querySelector('.movie_card');
-const searchForm = document.querySelector('.search_form');
-const searchInput = document.querySelector('#search_input')
-const resetBtn = document.querySelector('.reset_button');
-const searchLogContainer = document.querySelector('.search_log');
-const searchList = document.querySelector('.search_log_list');
+const movieContainer = document.querySelector('.movie-container');
+const movieCard = document.querySelector('.movie-card');
+const searchForm = document.querySelector('.search-form');
+const searchInput = document.querySelector('#search-input')
+const resetBtn = document.querySelector('.reset-button');
+const searchLogContainer = document.querySelector('.search-log');
+const searchList = document.querySelector('.search-log-list');
 const options = {
   method: 'GET',
   headers: {
@@ -16,21 +16,11 @@ const options = {
 // 초기 화면 그리는 함수
 fetchData()
 
-// 검색 기능
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  searchMovie(searchInput.value)
-  searchLog(searchInput.value)
-
-  searchInput.value = ''
-  searchLogContainer.id = ''
-})
 
 // 영화 카드 클릭 시 alert
-movieCard?.addEventListener('click', (e) => {
-  alert(`영화 id: ${e.target.dataset.id}`)
-})
-
+// movieCard?.addEventListener('click', (e) => {
+//   alert(`영화 id: ${e.target.dataset.id}`)
+// })
 
 // 초기화
 resetBtn.addEventListener('click', () => {
@@ -44,54 +34,82 @@ resetBtn.addEventListener('click', () => {
 // FUNCTION
 //==========
 
-// 데이터 fetch 후 초기 화면 그리기
+// 데이터 fetch 후 초기 화면 그리기 / 검색 기능
 async function fetchData() {
   const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?
   language=en-US&page=1`, options)
-  let data = await res.json();
 
-  // 카드 UI로 데이터 전달
-  createCard(data.results);
+  try {
+    let data = await res.json();
+    // 카드 UI로 데이터 전달
+    createCard(data.results);
+  }
+  catch (err) {
+    console.log(console.error(err));
+  }
+
+
+  // 검색 기능
+  searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // fetch 데이터와 인풋 입력 값을 전달하여 검색
+    searchMovie(data, searchInput.value)
+    // 인풋 입력 값을 전달하여 검색했던 목록 생성
+    searchedLog(searchInput.value)
+
+    searchInput.value = ''
+    searchLogContainer.id = ''
+  })
+
+  let cards = document.querySelectorAll('.movie-card');
+  showId(cards)
 }
 
 // 카드 UI 그리기
 function createCard(data) {
+  let tempHTML = '';
   data?.forEach(el => {
-    let tempHTML = data.length === 0
-      ? `<div><h1>찾는 정보가 없습니다</h1></div>`
-      :
-      `<div class="movie_card") data-id = '${el.id}' 
-    onclick = "alert('영화 id: ${el.id}')" >
-        <div class="movie_card_img">
-          <img src=${`https://image.tmdb.org/t/p/w300` + el.poster_path} alt="" />
+    if (data.length === 0) {
+      tempHTML = `<div><h1>찾는 정보가 없습니다</h1></div>`
+    } else {
+      tempHTML = `<div class="movie-card" data-id = '${el.id}'>
+        <div class="movie-card-img">
+          <img  data-id = '${el.id}' loading="lazy" src=${`https://image.tmdb.org/t/p/w300` + el.poster_path} alt="" />
         </div>
-        <h2 class="movie_card_title">${el.title}</h2>
-        <p class="movie_card_desc">
+        <h2 class="movie-card-title">${el.title}</h2>
+        <p class="movie-card-desc">
           ${el.overview}
         </p>
-        <h4 class='average_score'>Rating: ${el.vote_average}</h4>
+        <h4 class='average-score'>Rating: ${el.vote_average}</h4>
     </div >`
+    }
 
     movieContainer.insertAdjacentHTML('beforeend', tempHTML)
   })
+
 }
 
-// 검색 후 해당되는 데이터로 카드 UI 그리기
-async function searchMovie(searchValue) {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/top_rated?
-  language=en-US&page=1`, options)
-  let data = await res.json();
-  data = data.results;
+const showId = (el) => {
+  (Array.from(el).forEach(items => {
+    items.addEventListener('click', (e) => alert(`id: ${e.target.dataset.id}`))
+  }))
+}
 
-  let filtered = data.filter(el => (el.title.toLowerCase()).includes(searchValue.toLowerCase()))
+
+
+
+
+// 검색 후 해당되는 데이터로 카드 UI 그리기
+async function searchMovie(data, searchValue) {
+  let filtered = data.results.filter(el => (el.title.toLowerCase()).includes(searchValue.toLowerCase()))
   movieContainer.innerHTML = ``;
 
   createCard(filtered)
 }
 
-function searchLog(searchValue) {
-  let logHTML = `<li class="list_item" data-name=${searchValue}
-  onclick="(e) => searchByLog(e)">${searchValue}</li>`;
+function searchedLog(searchValue) {
+  let logHTML = `<li class="list_item" data-name=${searchValue}>
+  ${searchValue}</li>`;
 
   searchList.insertAdjacentHTML('beforeend', logHTML)
 }
